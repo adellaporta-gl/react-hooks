68c823e1f9d9bce43552ecd1b94b4ec529710725
adellaporta <adellaporta@hexacta.com>
2023-11-01 18:29:30 -0300

    Starting with React hooks course project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom";
+import { useState } from "react";
+import "./App.css";
+import { FaStar } from "react-icons/fa";
 
-function App() {
+// example using props
+function LastNameComponent(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <span className="lastName">{props.cValue}</span>
+  );
+}
+// example using props names => this is posible as props is just an Object => try it in the console 
+function FirstNameComponent({ cValue }) {
+  return (
+    <span className="firstName">{cValue}</span>
+  );
+}
+// Rating with stars
+function Star({ selected, onSelected }) {
+  return (
+    <FaStar
+      style={selected ? { "color": "red" } : { "color": "gray" }}
+      onClick={onSelected}
+    ></FaStar>
+  );
+}
+function StarRating() {
+  const [selectedStars, setSelectedStars] = useState(0);
+  const createArray = (length) => { return [...Array(length)] };
+  return createArray(5).map((n, i) => { return <Star key={i} selected={selectedStars > i} onSelected={() => setSelectedStars(i + 1)} /> });
+}
+
+export default function App() {
+  const [useStateConcept] = useState("This is the value of the initial 'name' variable using 'useState'");
+  return (
+    <div>
+      <p>Mi name is <FirstNameComponent cValue="Andrés" /> <LastNameComponent cValue="Della Porta" /></p>
+      <h3>useState Concept</h3><hr />
+      <p>{useStateConcept}</p>
+      <StarRating />
     </div>
   );
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
